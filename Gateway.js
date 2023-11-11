d403ac6c8a34eb8870f7600235efbaf705bb10b0
Junior Silveira <lilithering@gmail.com>
2023-11-10 21:24:01 -0300

    init

---
+++ Gateway.js
@@ -0,0 +1,26 @@
+const { cerr, cinfo } = require("./../GIT/LogManagement/LogManagement");
+const express = require("./../GIT/Lib/express");
+
+class GAMXGateway {
+    constructor() {
+        cinfo("Iniciando Gateway");
+        if (!this.Create()) return cerr("Falha ao tentar criar a rota");
+        if (!this.Defaults()) return cerr("Falha ao tentar definir os padrões da rota");
+
+        cinfo("Gateway iniciado com sucesso", { name: this.constructor.name });
+    }
+    Create() {
+        this.router = express.Router();
+
+        cinfo("Rota criada com sucesso");
+        return true;
+    }
+    Defaults() {
+        this.router.get("/", (req, res) => { res.send("Hello World! Message from Gateway Router.") });
+
+        cinfo("Padrões definidos com sucesso");
+        return true;
+    }
+};
+
+module.exports = { GAMXGateway };
